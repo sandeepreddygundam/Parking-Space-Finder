c09e1e6b08b1c8d32ef45f2482772ef64397a02d
Hardeep111 <105501123+Hardeep111@users.noreply.github.com>
2022-05-13 10:04:09 -0400

    Add files via upload

---
+++ ADMIN/admin.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+include 'config.php';
+
+$uname = $_POST['uname'];
+$pass = $_POST['pass'];
+
+$sql = "SELECT email,pass FROM admin where email='$uname' and pass='$pass' ";
+$result = $conn->query($sql);
+
+
+//$_SESSION["email"] = $uname;
+
+
+if ($result->num_rows > 0) {
+
+  // fetch data
+  $id = mysqli_fetch_array($result);
+
+  //   echo "Error: " . $sql . "<br>" . $conn->error;
+  header('Location:adminhome.php');
+} else {
+  echo "<script>alert('Invalid Details');document.location='index.html'</script>";
+}
+$conn->close();
+?>
