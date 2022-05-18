9922b1c4d56c5cffb68bb080983eb6a6f823242a
sandeepreddygundam <94187006+sandeepreddygundam@users.noreply.github.com>
2022-05-18 09:25:49 -0400

    Update login.php

--- USER/login.php
+++ USER/login.php
@@ -6,7 +6,7 @@ include 'config.php';
 $uname = $_POST['uname'];
 $pass = $_POST['pass'];
 
-$sql = "SELECT email,pass FROM user where email='$uname' and pass='$pass' ";
+$sql = "SELECT email,pass FROM user where email='$uname' and pass='$pass' and status='Active' ";
 $result = $conn->query($sql);
 
 
@@ -21,7 +21,7 @@ if ($result->num_rows > 0) {
   //   echo "Error: " . $sql . "<br>" . $conn->error;
   header('Location:home.php');
 } else {
-  echo "<script>alert('Invalid Details');document.location='index.html'</script>";
+  echo "<script>alert('Invalid Details or your account has been blocked');document.location='login.html'</script>";
 }
 $conn->close();
 ?>
