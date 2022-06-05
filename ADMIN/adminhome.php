f2df1f03cfc34e9eb1f85edba3451c09dc2b83be
dileepkumarreddy16 <67062585+dileepkumarreddy16@users.noreply.github.com>
2022-06-05 12:50:09 -0400

    Update adminhome.php

--- ADMIN/adminhome.php
+++ ADMIN/adminhome.php
@@ -71,8 +71,163 @@
           
           <h1 style"text-align:center;margin-top:200px">Welcome To Admin Dashboard</h1>
       </div>
-  </div>
-</div>
 
-</body>
-</html>
+        <?php include('config.php'); ?>
+
+<div class="row">
+
+<div class="col-xl-3 col-md-6">
+
+<div class="card bg-primary text-white mb-4"> <div class="card-body">
+
+Total number of parking slots
+
+<?php
+
+$dash_parking_query = "SELECT * from parking";
+$dash_parking_query_run = mysqli_query($conn, $dash_parking_query);
+
+if ($parking_total = mysqli_num_rows($dash_parking_query_run))
+{
+echo '<h4 class="mb-0"> '.$parking_total.' </h4>';
+}
+else
+{
+
+echo '<h4 class="mb-8"> No Data </h4>';
+}
+?>
+
+</div>
+</div>
+</div>
+<div class="row">
+
+<div class="col-xl-3 col-md-6">
+
+<div class="card bg-primary text-white mb-4"> <div class="card-body">
+
+Total number of booked parking slots
+
+<?php
+
+$dash_parking_booked_query = "SELECT * from parking  WHERE status='Booked'";
+$dash_parking_booked_query_run = mysqli_query($conn, $dash_parking_booked_query );
+
+if ($parkingbooked_total = mysqli_num_rows($dash_parking_booked_query_run))
+{
+echo '<h4 class="mb-0"> '.$parkingbooked_total.' </h4>';
+}
+else
+{
+
+echo '<h4 class="mb-8"> No Data </h4>';
+}
+?>
+
+</div>
+</div>
+</div>
+<div class="row">
+
+<div class="col-xl-3 col-md-6">
+
+<div class="card bg-primary text-white mb-4"> <div class="card-body">
+
+Total number of available parking slots
+
+<?php
+
+$dash_parking_open_query = "SELECT * from parking  WHERE status='Open Now'";
+$dash_parking_open_query_run = mysqli_query($conn, $dash_parking_open_query );
+
+if ($parkingopen_total = mysqli_num_rows($dash_parking_open_query_run))
+{
+echo '<h4 class="mb-0"> '.$parkingopen_total.' </h4>';
+}
+else
+{
+
+echo '<h4 class="mb-8"> No Data </h4>';
+}
+?>
+
+</div>
+</div>
+</div>
+<div class="row">
+
+<div class="col-xl-3 col-md-6">
+
+<div class="card bg-primary text-white mb-4"> <div class="card-body">
+
+Total number of users
+
+<?php
+
+$dash_user_query = "SELECT * from user";
+$dash_user_query_run = mysqli_query($conn, $dash_user_query );
+
+if ($user_total = mysqli_num_rows($dash_user_query_run))
+{
+echo '<h4 class="mb-0"> '.$user_total.' </h4>';
+}
+else
+{
+
+echo '<h4 class="mb-8"> No Data </h4>';
+}
+?>
+
+</div>
+</div>
+</div>
+<div class="col-xl-3 col-md-6">
+
+<div class="card bg-primary text-white mb-4"> <div class="card-body">
+
+Total number of active users
+
+<?php
+
+$dash_user_active_query = "SELECT * from user WHERE status='Active'";
+$dash_user_active_query_run = mysqli_query($conn, $dash_user_active_query);
+
+if ($useractive_total = mysqli_num_rows($dash_user_active_query_run))
+{
+echo '<h4 class="mb-0"> '.$useractive_total.' </h4>';
+}
+else
+{
+
+echo '<h4 class="mb-8"> No Data </h4>';
+}
+?>
+
+</div>
+</div>
+</div>
+<div class="col-xl-3 col-md-6">
+<div class="card bg-primary text-white mb-4"> <div class="card-body">
+
+Total number of blocked users
+
+<?php
+
+$dash_user_blocked_query = "SELECT * from user WHERE status='Blocked'";
+$dash_user_blocked_query_run = mysqli_query($conn, $dash_user_blocked_query);
+
+if ($userblocked_total = mysqli_num_rows($dash_user_blocked_query_run))
+{
+echo '<h4 class="mb-0"> '.$userblocked_total.' </h4>';
+}
+else
+{
+
+echo '<h4 class="mb-8"> No Data </h4>';
+}
+?>
+
+</div>
+</div>
+</div>
